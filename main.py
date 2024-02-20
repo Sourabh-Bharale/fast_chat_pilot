6273142093d56267bb69a5b411a523151af52365
Sourabh-Bharale <sourabhbharale80@gmail.com>
2024-02-20 14:28:08 +0530

    chore: setup cors for local development

--- main.py
+++ main.py
@@ -9,8 +9,23 @@ from langchain_community.document_loaders import PyPDFLoader
 from dotenv import load_dotenv
 from langchain.text_splitter import CharacterTextSplitter
 from qdrant_client.models import VectorParams, Distance
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 load_dotenv()
+
+origins = [
+    "http://localhost:3000",  # Allow frontend to connect from this origin
+    # Add more origins if needed
+]
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # create a Qdrant client
 qdrant_client = QdrantClient(
@@ -30,15 +45,18 @@ def create_collection():
     )
 
 # TODO: Add a file upload endpoint
-@app.get("/upload")
-def upload_file(url:str = Query(...)):
-    pdf_loader = PyPDFLoader(url)
-    documents = pdf_loader.load()
+class UploadFile(BaseModel):
+    file_url:str
 
+@app.post("/upload")
+def upload_file(file: UploadFile):
+    pdf_loader = PyPDFLoader(file.file_url)
+    print(file.file_url)
+    documents = pdf_loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
     docs = text_splitter.split_documents(documents)
 
-    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAIAPI_KEY"))
     
     qdrant = Qdrant.from_documents(
         docs,
@@ -49,17 +67,18 @@ def upload_file(url:str = Query(...)):
         collection_name="my_documents"
     )
 
-    return {"url": url, "documents": docs}
+    return {"message": "Document Upserted to VectorDB"}
+
 
 @app.get("/query")
 def search_query(query:str = Query(...)):
     # search for the query in the Qdrant
-    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAIAPI_KEY"))
     query_embeddings = embeddings.embed_query(query)
     res = qdrant_client.search(
         collection_name="my_documents",
         query_vector=query_embeddings,
-        limit=5
+        limit=1
     )
 
     query_params = "The query is: " + query
